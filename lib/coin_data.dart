9bdb95e16920434cb9cb8fd242513ec098e4bb35
toksoykaan <toksoykaan@gmail.com>
2022-10-16 16:57:29 +0300

    Starting Project is imported. API connection between coinapi.io is established.
    Successfuly retrieved first bitcoin price and delivered it to the main screen.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'price_screen.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,14 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  final double exchangeValue;
+
+  const CoinData({this.exchangeValue});
+
+  factory CoinData.fromJson(Map<String, dynamic> json) {
+    return CoinData(
+      exchangeValue: json['rate'],
+    );
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,13 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
+import 'package:http/http.dart' as http;
+import 'package:bitcoin_ticker/price_screen.dart';
+import 'dart:convert';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +15,52 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurreny = 'USD';
+  double currencyPrice = 5.0;
+  DropdownButton<String> dropDownButton() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      String curreny = currenciesList[i];
+      var newItem = DropdownMenuItem(
+        child: Text(curreny),
+        value: curreny,
+      );
+      dropDownItems.add(newItem);
+    }
+    return DropdownButton<String>(
+      value: selectedCurreny,
+      items: dropDownItems,
+      onChanged: (value) {
+        this.setState(() {
+          selectedCurreny = value;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iosPicker() {
+    List<Widget> cupertinoItems = [];
+    for (int i = 0; i < currenciesList.length; i++) {
+      String curreny = currenciesList[i];
+      cupertinoItems.add(Text(curreny));
+    }
+    return CupertinoPicker(
+        itemExtent: 32.0,
+        onSelectedItemChanged: (selectedIndex) {
+          setState(() {
+            selectedCurreny = currenciesList[selectedIndex];
+          });
+        },
+        children: cupertinoItems);
+  }
+
+  Future<CoinData> fetchCoin() async {
+    final response = await http.get(Uri.parse(
+        'https://rest.coinapi.io/v1/exchangerate/BTC/$selectedCurreny?apikey=6108B336-61C2-4EAD-A7E4-95D99E276B6A'));
+    currencyPrice = CoinData.fromJson(jsonDecode(response.body)).exchangeValue;
+    return CoinData.fromJson(jsonDecode(response.body));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +82,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $currencyPrice USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -38,12 +93,11 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? iosPicker() : dropDownButton()),
         ],
       ),
     );
